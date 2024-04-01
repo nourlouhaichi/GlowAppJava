89790b9207b7ff4fbb33d94af0ae08b9f9751889
nourlouhaichi <Nour.Louhaichi@esprit.tn>
2024-04-01 22:11:46 +0100

    database connexion

---
+++ src/main/java/Tests/main.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import Utils.MyDatabase;
+
+public class main extends Object {
+
+    public static void main(String[] args) {
+
+      MyDatabase bd= MyDatabase.getInstance();
+    }
+}
---
+++ src/main/java/Utils/MyDatabase.java
@@ -0,0 +1,35 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDatabase {
+    private final String URL = "jdbc:mysql://localhost:3306/glowapp";
+    private final String USER = "root";
+    private final String PASSWORD = "";
+    private Connection connection;
+    private static MyDatabase instance;
+
+    private MyDatabase() {
+        try {
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected");
+        } catch (SQLException var2) {
+            System.err.println(var2.getMessage());
+        }
+
+    }
+
+    public static MyDatabase getInstance() {
+        if (instance == null) {
+            instance = new MyDatabase();
+        }
+
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
